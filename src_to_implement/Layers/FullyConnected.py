375bdaf26eae6da99b2dd87407648478babf039b
GAlibrz <gitalibagherzadeh@gmail.com>
2023-11-23 21:33:42 +0100

    fix: no tensorflow is used in FullyConnected

--- src_to_implement/Layers/FullyConnected.py
+++ src_to_implement/Layers/FullyConnected.py
@@ -1,6 +1,4 @@
 import numpy as np
-import tensorflow as tf
-#from Base import BaseLayer
 from Optimization import Optimizers
 
 
@@ -27,14 +25,16 @@ class FullyConnected(BaseLayer):
         self.input = None
 
         # Use tf.random.normal for weight initialization
-        self.weights_no_biases = tf.random.normal((input_size, output_size))
-        self.biases = tf.zeros((output_size,))
-        self.weights = tf.concat([self.weights_no_biases, self.biases], axis=0)
+        self.weights_no_biases = np.random.normal(size = (input_size, output_size))
+        #print(input_size, '  ', output_size, self.weights_no_biases.shape)
+        self.biases = np.zeros((1, output_size))
+        self.weights = np.concatenate([self.weights_no_biases, self.biases], axis=0)
         self._optimizer = None
 
     def forward(self, input_tensor):
+        #print(self.trainable)
         self.input = input_tensor
-        result_without_biases = tf.matmul(input_tensor, self.weights)
+        result_without_biases = np.dot(input_tensor, self.weights_no_biases)
         result_with_biases = result_without_biases + self.biases
         return result_with_biases
 
@@ -51,11 +51,11 @@ class FullyConnected(BaseLayer):
         return self._gradient_weights
 
     def backward(self, error_tensor):
-        self.gradient_weights_without_biases = tf.matmul(tf.transpose(self.input), error_tensor)
-        self.biases_gradient = tf.reduce_sum(error_tensor, axis=0)
+        self.gradient_weights_without_biases = np.dot(np.transpose(self.input), error_tensor)
+        self.biases_gradient = np.sum(error_tensor, axis=0)
 
         # Concatenate weights and biases into one matrix
-        self._gradient_weights = tf.concat([self.gradient_weights_without_biases, self.biases_gradient], axis=0)
+        self._gradient_weights = np.concatenate([self.gradient_weights_without_biases, self.biases_gradient], axis=0)
 
         # Assuming self._optimizer is an instance of the Optimizers class
         self.weights = self._optimizer.calculate_update(self.weights_and_biases, self._gradient_weights)
--- src_to_implement/test.py
+++ src_to_implement/test.py
@@ -7,11 +7,10 @@ import matplotlib.pyplot as plt
 import tabulate
 import argparse
 
-layer = FullyConnected.FullyConnected(input_size, output_size)
 
 input_size = 4
 batch_size = 9
 output_size = 3
 input_tensor = np.random.rand(batch_size, input_size)
-layer = FullyConnected(4, 3)
+layer = FullyConnected.FullyConnected(input_size, output_size)
 layer.forward(input_tensor)

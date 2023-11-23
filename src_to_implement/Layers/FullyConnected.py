4eb24b04c6e9a8aa2089ceed1726b3c02262e3d4
GAlibrz <gitalibagherzadeh@gmail.com>
2023-11-23 22:21:44 +0100

    last push of night

--- src_to_implement/Layers/FullyConnected.py
+++ src_to_implement/Layers/FullyConnected.py
@@ -26,9 +26,11 @@ class FullyConnected(BaseLayer):
 
         # Use tf.random.normal for weight initialization
         self.weights_no_biases = np.random.normal(size = (input_size, output_size))
+        self.weights_no_biases = (self.weights_no_biases - np.min(self.weights_no_biases)) / (np.max(self.weights_no_biases) - np.min(self.weights_no_biases))
         #print(input_size, '  ', output_size, self.weights_no_biases.shape)
         self.biases = np.random.normal(size = (1, output_size))
-        self.weights = np.concatenate([self.weights_no_biases, self.biases], axis=0)
+        self.biases = (self.biases - np.min(self.biases)) / (np.max(self.biases) - np.min(self.biases))
+        self.weights = np.concatenate((self.weights_no_biases, self.biases), axis=0)
         self._optimizer = None
         print(self.weights.shape)
 
@@ -52,13 +54,20 @@ class FullyConnected(BaseLayer):
         return self._gradient_weights
 
     def backward(self, error_tensor):
+        print("error tensor shape", error_tensor.shape)
         self.gradient_weights_without_biases = np.dot(np.transpose(self.input), error_tensor)
+        print("gradient no biases (after doting input and error): ", self.gradient_weights_without_biases.shape)
         self.biases_gradient = np.sum(error_tensor, axis=0)
+        self.biases_gradient = self.biases_gradient.reshape(1, -1)
+        print("bias: ")
+        print(self.biases_gradient.shape)
 
         # Concatenate weights and biases into one matrix
-        self._gradient_weights = np.concatenate([self.gradient_weights_without_biases, self.biases_gradient], axis=0)
+        self._gradient_weights = np.concatenate((self.gradient_weights_without_biases, self.biases_gradient), axis=0)
 
         # Assuming self._optimizer is an instance of the Optimizers class
-        self.weights = self._optimizer.calculate_update(self.weights_and_biases, self._gradient_weights)
-        self.weights_no_biases = self.weights_and_biases[:-1, :]
-        self.biases = self.weights_and_biases[-1, :]
+        if self._optimizer:
+            self.weights = self._optimizer.calculate_update(self.weights, self._gradient_weights)
+            self.weights_no_biases = self.weights[:-1, :]
+            self.biases = self.weights[-1, :]
+        return self._gradient_weights

5368076b8219ca3054364c8127621e6b817ae339
GAlibrz <gitalibagherzadeh@gmail.com>
2023-11-23 20:31:23 +0100

    fix: Base was added to all files by hard copying

--- src_to_implement/Layers/FullyConnected.py
+++ src_to_implement/Layers/FullyConnected.py
@@ -1,7 +1,21 @@
 import numpy as np
 import tensorflow as tf
-from Base import BaseLayer
+#from Base import BaseLayer
 from Optimization import Optimizers
+
+
+class BaseLayer:
+    def __init__(self):
+        self.trainable = False
+
+    def forward(self, input_tensor):
+
+        raise NotImplementedError("Forward pass must be implemented in derived classes.")
+
+    def backward(self, error_tensor):
+
+        raise NotImplementedError("Backward pass must be implemented in derived classes.")
+
 
 class FullyConnected(BaseLayer):
 

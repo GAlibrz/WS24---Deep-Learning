eee9e1bdd23984550e02e12c477f49fd795a4a9c
Mohammad Madahian <madahian@Mohammads-MacBook-Pro.local>
2023-11-24 22:56:15 +0100

    fix: Loss.py refactor variable

--- src_to_implement/Optimization/Loss.py
+++ src_to_implement/Optimization/Loss.py
@@ -6,9 +6,9 @@ class CrossEntropyLoss:
         super().__init__()
         self.prediction_store = None
 
-    def forward(self, input_tensor, label_tensor):
-        self.prediction_store = input_tensor
-        return np.sum(-np.log(input_tensor[label_tensor == 1] + np.finfo(float).eps))
+    def forward(self, prediction_tensor, label_tensor):
+        self.prediction_store = prediction_tensor
+        return np.sum(-np.log(prediction_tensor[label_tensor == 1] + np.finfo(float).eps))
 
     def backward(self, label_tensor):
         return -np.divide(label_tensor, self.prediction_store)
